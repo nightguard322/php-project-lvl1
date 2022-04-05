1ef6ae92e1e1bec9d37eea8a7fb2b040d361c420
sander <sander_@inbox.ru>
2022-04-05 15:13:17 +0300

    new  makefile

--- src/Cli.php
+++ src/Cli.php
@@ -1,18 +1,14 @@
 <?php
+
 namespace Brain\Games\Cli;
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1; 
-}else {
-    require_once $autoloadPath2; 
-}
+
+require_once __DIR__ . '/../vendor/autoload.php';
 use function cli\line;
 use function cli\prompt;
+
 function makeHello()
 {
-	\cli\line('Welcome to Brain Game!');
-	$name = \cli\prompt('May I have your name?');
-	\cli\line("Hello, %s", $name);
+    \cli\line('Welcome to Brain Game!');
+    $name = \cli\prompt('May I have your name?');
+    \cli\line("Hello, %s", $name);
 }
-
